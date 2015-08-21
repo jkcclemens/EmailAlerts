06b1b4d571aceb1a31ba8e5511daa9cedf3daaf8
Kyle Clemens <jkc.clemens@gmail.com>
2015-08-21 01:05:19 -0400

    Updated: Fuck it

--- resources/views/authed/index.blade.php
+++ resources/views/authed/index.blade.php
@@ -16,8 +16,7 @@
         </div>
     @endif
     <p>
-        Set your email provider up to forward emails that you want as notifications to
-        <em>receive{{ '@' . url() }}</em>.
+        Set your email provider up to forward emails that you want as notifications to <em>receive@emailalerts.xyz</em>.
         Received emails will appear below!
     </p>
     @if(Auth::user()->notifications()->count() < 1)
